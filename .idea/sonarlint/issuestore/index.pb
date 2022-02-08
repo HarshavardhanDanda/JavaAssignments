033ef36cf67db17614a53d3bdc0cebc30468773d
Harshavardhan Reddy Danda <harshavardhan.danda@zemosolabs.com>
2022-02-08 15:44:56 +0530

    String contains all letters from a to z

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+M
+src/com/assignments/Main.java,d\e\de985656d40c155f4731fa52daedc713f4b1a755
---
+++ src/com/assignments/Assignment2.java
@@ -0,0 +1,37 @@
+package com.assignments;
+import java.util.Scanner;
+
+public class Assignment2 {
+
+    public static void main(String[] args) {
+         String str;
+         System.out.println("Enter any String");
+         Scanner input=new Scanner(System.in);
+         str=input.nextLine();
+         String lower= str.toLowerCase();
+         boolean b=checkAlpha(lower);
+         if(b){
+             System.out.println("Yes, the string contains all letters from a to z");
+         }
+         else{
+             System.out.println("No, the string does not contains all letters from a to z");
+         }
+
+    }
+    public static boolean checkAlpha(String str){
+        int[] myArray=new int[26];
+
+        for(int i=0;i<str.length();i++){
+            int ascii=(int)str.charAt(i);
+            myArray[ascii-97]=1;
+        }
+
+        for(int i=0;i<26;i++){
+             if(myArray[i]==0){
+                 return(false);
+             }
+        }
+        return(true);
+        }
+    }
+
