acd0a7915b60be9ce72d3227626996a9fdbb6e5d
Harshavardhan Reddy Danda <harshavardhan.danda@zemosolabs.com>
2022-02-11 00:02:21 +0530

    Initial Commit

---
+++ src/Assignment6/Vampire.java
@@ -0,0 +1,7 @@
+package Assignment6;
+
+public class Vampire {
+    public static void main(String[] args){
+
+    }
+}
--- src/com/assignments/Assignment8.java
+++ src/com/assignments/Assignment8.java
@@ -1,4 +1,42 @@
 package com.assignments;
 
 public class Assignment8 {
+    public static void CreateException(String name, char gender, int age) throws nameException, genderException, ageException{
+        if (!name.equals("harsha")){
+            throw new nameException();
+        }
+        if (gender!=('M')){
+            throw new genderException();
+        }
+        if (age!=21){
+            throw new ageException();
+        }
+
+    }
+    public static void main(String[] args){
+        try{
+            CreateException("mani",'F',20);
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+        }
+        finally{
+            System.out.println("this is finally block");
+        }
+    }
+}
+class nameException extends Exception{
+    public String toString(){
+        return ("your name is harsha");
+    }
+}
+class genderException extends Exception{
+    public String toString(){
+        return("your gender is male");
+    }
+}
+class ageException extends Exception{
+    public String toString(){
+        return("your age is 21");
+    }
 }
