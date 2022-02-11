7fa210165c0d9d8e3b95e9d07fe909f4183ab3d2
Harshavardhan Reddy Danda <harshavardhan.danda@zemosolabs.com>
2022-02-11 09:53:10 +0530

    Initial Commit

---
+++ src/com/assignments/Assignment1.java
@@ -0,0 +1,39 @@
+package com.assignments;
+import java.io.*;
+import java.util.regex.*;
+import java.util.*;
+
+public class Assignment1 {
+
+    private static final File homeDirectory=new File("C:\\zemoso");
+
+
+    public static void checkMatchingFiles()
+    {
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the file name to match OR Enter exit to exit");
+            String input = scanner.nextLine();
+            if(input.equals("exit"))
+                return;
+            Pattern pattern = Pattern.compile(input, Pattern.CASE_INSENSITIVE);
+            printAbsolutePath(homeDirectory, pattern);
+        }
+    }
+    public static void printAbsolutePath(File homeDirectory,Pattern pattern){
+        File[] Files = homeDirectory.listFiles();
+        for (File file : Files) {
+            if (file.isDirectory())
+                printAbsolutePath(file,pattern);
+            else {
+                Matcher matcher = pattern.matcher(file.getName());
+                if(matcher.find())
+                    System.out.println(file.getAbsolutePath());
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        checkMatchingFiles();
+    }
+}
